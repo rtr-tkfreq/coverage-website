200b676dbaaaebc2225519f775a75e0fadee7dd4
Thomas Schreiber <thomas.schreiber@gmx.at>
2021-07-08 17:01:29 +0200

    initial map commit

--- src/app/frqmap/sample1.ts
+++ src/app/frqmap/sample1.ts
@@ -2,11 +2,13 @@ import {Form} from "@angular/forms";
 
 export interface FormOptionResponse {
   filter: Filter,
+
   [key: string]: any
 }
 
 export interface Filter {
   operators: Array<Operator>,
+
   [key: string]: any
 }
 
@@ -16,83 +18,92 @@ export interface Operator {
   operator: string | null
 }
 
-const sample : FormOptionResponse  ={
+export interface LayerConfiguration {
+  operator: string,
+  reference: string,
+  date: string,
+  url: string
+
+  [key: string]: any
+}
+
+const sample: FormOptionResponse = {
   "object": "settings",
   "filter": {
-  "bands": [
-    {
-      "name": "@all",
-      "default": true,
-      "reference": "F1/16"
-    },
-    {
-      "name": "@3400mhz",
-      "default": false,
-      "reference": "F7/16"
-    }
-  ],
+    "bands": [
+      {
+        "name": "@all",
+        "default": true,
+        "reference": "F1/16"
+      },
+      {
+        "name": "@3400mhz",
+        "default": false,
+        "reference": "F7/16"
+      }
+    ],
     "timeline": [
-    {
-      "date": "2021-01-01"
-    },
-    {
-      "date": "2021-01-03"
-    },
-    {
-      "date": "2021-03-05"
-    },
-    {
-      "date": "2021-06-30"
-    }
-  ],
+      {
+        "date": "2021-01-01"
+      },
+      {
+        "date": "2021-01-03"
+      },
+      {
+        "date": "2021-03-05"
+      },
+      {
+        "date": "2021-06-30"
+      }
+    ],
     "operators": [
-    {
-      "label": "@all",
-      "default": true,
-      "operator": null
-    },
-    {
-      "label": "A1 Telekom Austria",
-      "default": false,
-      "operator": "A1TA"
-    },
-    {
-      "label": "T-Mobile Austria",
-      "default": false,
-      "operator": "TMA"
-    },
-    {
-      "label": "Hutchison Drei Austria",
-      "default": false,
-      "operator": "H3A"
-    },
-    {
-      "label": "LIWEST Kabel Medien",
-      "default": false,
-      "operator": "LIWEST"
-    },
-    {
-      "label": "Salzburg AG",
-      "default": false,
-      "operator": "SBAG"
-    },
-    {
-      "label": "Mass Response",
-      "default": false,
-      "operator": "MASS"
-    },
-    {
-      "label": "Graz Holding",
-      "default": false,
-      "operator": "GRAZH"
-    }
-  ]
-}
+      {
+        "label": "@all",
+        "default": true,
+        "operator": null
+      },
+      {
+        "label": "A1 Telekom Austria",
+        "default": false,
+        "operator": "A1TA"
+      },
+      {
+        "label": "T-Mobile Austria",
+        "default": false,
+        "operator": "TMA"
+      },
+      {
+        "label": "Hutchison Drei Austria",
+        "default": false,
+        "operator": "H3A"
+      },
+      {
+        "label": "LIWEST Kabel Medien",
+        "default": false,
+        "operator": "LIWEST"
+      },
+      {
+        "label": "Salzburg AG",
+        "default": false,
+        "operator": "SBAG"
+      },
+      {
+        "label": "Mass Response",
+        "default": false,
+        "operator": "MASS"
+      },
+      {
+        "label": "Graz Holding",
+        "default": false,
+        "operator": "GRAZH"
+      }
+    ]
+  }
 }
 
 export default sample;
 
-let demo : Operator;
+let demo: Operator;
 demo = {
   "label": "@all",
   "default": true,
